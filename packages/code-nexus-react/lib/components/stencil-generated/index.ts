5b6f77ee479c2e7d34a272b62a290d93d684c032
rleal1995@hotmail.com <rleal1995@hotmail.com>
2023-10-04 14:01:05 -0400

    add turborepo and implement it inside of repo

--- packages/code-nexus-react/lib/components/stencil-generated/index.ts
+++ packages/code-nexus-react/lib/components/stencil-generated/index.ts
@@ -1,9 +1,11 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from '../..';
+import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from 'code-nexus';
+
+
 
 export const CodeNexus = /*@__PURE__*/createReactComponent<JSX.CodeNexus, HTMLCodeNexusElement>('code-nexus');
 export const NexusPanel = /*@__PURE__*/createReactComponent<JSX.NexusPanel, HTMLNexusPanelElement>('nexus-panel');
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
+# Code Nexus
 ![image](https://github.com/rafael-leal-mccormack/code-sandbox/assets/5799157/9e7da93f-a24f-445c-ad1c-7a6cb46f747c)
+
+## Running code nexus demo env
+
+To run the code nexus demo environment, you should run:
+
+> npm ci
+
+> npm run start:nexus
+
+
+## Running the code nexus web site
+
+To run the code nexus documentation site, you should run:
+
+*Note: only do the install command if you havent run already*
+> npm ci
+
+> npm run start:web
