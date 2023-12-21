8c82e86ff54fb31ba8509dd45b44512970d1ac03
rleal1995@hotmail.com <rleal1995@hotmail.com>
2023-12-20 22:28:17 -0500

    add markdown component and first markdown file for roboto

--- packages/code-nexus-web/src/app/getting-started/page.tsx
+++ packages/code-nexus-web/src/app/getting-started/page.tsx
@@ -1,7 +1,17 @@
-'use client'
+import { readFileSync } from "fs";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-export default function Examples() {
-  return (<div className="w-full h-full">
-    {/* <CodeNexus className="h-full"></CodeNexus> */}
-  </div>)
-};
+export default function GettingStarted() {
+  const markdown = readFileSync(
+    process.cwd() + "/public/assets/roboto-instructions.md",
+    "utf8"
+  );
+  return (
+    <div className="w-full h-full">
+      hello
+      {/* @ts-expect-error Server Component */}
+      <MDXRemote source={markdown} />
+      {/* <CodeNexus className="h-full"></CodeNexus> */}
+    </div>
+  );
+}
