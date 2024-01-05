0cfcf318f71791a820384a52a438a4509107b540
rleal1995@hotmail.com <rleal1995@hotmail.com>
2024-01-05 01:05:02 -0500

    trying out fix for build

--- packages/code-nexus-web/src/components/nexus-example.tsx
+++ packages/code-nexus-web/src/components/nexus-example.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { CodeNexus } from "code-nexus-react"
+import { CodeNexus } from "../../../code-nexus-react"
 
 export default function NexusExample() {
   return (<CodeNexus></CodeNexus>)
